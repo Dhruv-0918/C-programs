ec04eeaad5e44c747cf0ff09fbbe9ca2b805cf34
Dhruv-0918 <dhruv.chourasiya18@gmail.com>
2025-10-31 10:29:55 +0530

    recursion

---
+++ palindrome.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// sum of 1 to 5 using recursion
+
+int palindrome(int arr[],int start,int end  ){
+    if(start>=end){
+        return 1;
+    }
+    if(arr[start]==arr[end]){
+        return palindrome(arr,start+1,end-1);
+    }
+    else return 0;
+    
+ }
+int main()
+{
+    int i=0;
+    int j=2;
+   int arr[3]={1,2,1};
+   if(palindrome(arr,i,j)){
+    printf("Palindrome");
+   }
+   else{
+    printf("not palindrome");
+   }
+   return 0;
+}
+   
+   
